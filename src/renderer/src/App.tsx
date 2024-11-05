d3ce24408dd5b79c0a7bf88292a5665cd31fab39
Matheus-Bezera <matheusbwzerra002@gmail.com>
2024-11-05 17:01:08 -0300

    Ajustando SHADCN

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,20 @@
 import { Button } from '../src/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from './components/ui/avatar';
+import { Input } from './components/ui/input';
 import './styles/global.css'
 
 export function App(): JSX.Element {
   return (
-    <div className='bg-primary text-primary h-[100vh]'>
-      <Button variant={"destructive"}>teste</Button>
-    </div>
+    <div className='bg-foreground h-[100vh]'>
+      <Button>teste</Button> 
 
+      <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+      <Input />
+    </div>
+ 
   )
 }
- 
+  
