b9bd063c6a7d530f831ad87c9c0a42afafc40645
Matheus-Bezera <matheusbwzerra002@gmail.com>
2024-11-06 09:58:45 -0300

    Resolve conflito

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,13 +4,5 @@ import './styles/global.css'
 import { router } from './routes';
 
 export function App(): JSX.Element {
-<<<<<<< HEAD
   return <RouterProvider router={router} />;
-=======
-  return (
-    <>
-      <h1 className="text-violet-400 text-2xl">hello</h1>
-    </>
-  )
->>>>>>> 4b41261f7fc290be288bf95d7e1362f40ede06a3
 }
