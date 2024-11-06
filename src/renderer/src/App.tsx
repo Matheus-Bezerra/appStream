f7fb9879134e6d6673138c89fc65a0ec3ac87e0b
Matheus-Bezera <matheusbwzerra002@gmail.com>
2024-11-06 09:51:50 -0300

    Removendo imports

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,6 @@
-import { Button } from '../src/components/ui/button';
-import { Avatar, AvatarFallback, AvatarImage } from './components/ui/avatar';
-import { Input } from './components/ui/input';
-import { BrowserRouter as Router, Routes, Route, RouterProvider } from "react-router-dom";
+
+import {RouterProvider } from "react-router-dom";
 import './styles/global.css'
-import { Home } from './pages/Main/Home';
 import { router } from './routes';
 
 export function App(): JSX.Element {
