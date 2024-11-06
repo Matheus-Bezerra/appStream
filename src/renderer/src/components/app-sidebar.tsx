599cc7bee78e10f3ec4df520cf94597c2d508228
bielfreire <cf_biel@hotmail.com>
2024-11-06 18:11:08 -0300

    sidebar com flowbite

---
+++ src/renderer/src/components/app-sidebar.tsx
@@ -0,0 +1,52 @@
+import { Home, Inbox, Calendar, Search, Settings } from "lucide-react"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+  } from "../components/ui/sidebar"
+  
+
+// Menu items.
+const items = [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Inbox",
+      url: "#",
+      icon: Inbox,
+    },
+    {
+      title: "Calendar",
+      url: "#",
+      icon: Calendar,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ]
+   
+  export function AppSidebar() {
+    return (
+        <Sidebar>
+        <h2>Menu</h2>
+        {/* Adicione itens de navegação ou outras funcionalidades */}
+      </Sidebar>
+    )
+  }
