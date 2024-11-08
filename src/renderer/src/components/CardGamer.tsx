e2541f33d4e62eaa53a7b735e16f66c621130ba5
Matheus-Bezera <matheusbwzerra002@gmail.com>
2024-11-08 17:24:22 -0300

    Refatorando componente card

--- src/renderer/src/components/CardGamer.tsx
+++ src/renderer/src/components/CardGamer.tsx
@@ -1,32 +1,34 @@
-// components/CardGamer.tsx
 import React from "react";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 interface CardGamerProps {
   titulo: string;
   imagem: string;
-  limk?: string
+  link?: string
 }
 
-export const CardGamer: React.FC<CardGamerProps> = ({ titulo, imagem, limk }) => {
+export const CardGamer: React.FC<CardGamerProps> = ({ titulo, imagem, link = "/"}) => {
   return (
-    <Card className="bg-foreground p-0 border-0 rounded-2xl max-w-md cursor-pointer">
-      <CardContent className="flex flex-col items-center">
-        <img
-          src={imagem}
-          alt={titulo}
-          className="w-full h-32 object-cover rounded-lg mb-4"
-        />
-        <h3 className="text-primary font-bold text-center mb-2">
-          {titulo}
-        </h3>
-        <div className="w-full max-w-60">
-          <Button variant="outline" className="w-full">
-            Acessar
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+    <Link to={link} className="group">
+      <Card className="bg-foreground p-0 border-0 rounded-2xl max-w-md cursor-pointer">
+        <CardContent className="flex flex-col items-center">
+          <img
+            src={imagem}
+            alt={titulo}
+            className="w-full h-32 object-cover rounded-lg mb-4"
+          />
+          <h3 className="text-primary font-bold text-center mb-2">
+            {titulo}
+          </h3>
+          <div className="w-3/4 max-w-60">
+            <Button variant="outline" className="w-full transition-all group-hover:bg-primary group-hover:text-black group-hover:border-black">
+              Acessar
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
